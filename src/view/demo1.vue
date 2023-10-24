b944bf329a60f1b95f71cbc03a996e55b40b59e9
chensyouuuu <1094790357@qq.com>
2023-10-24 15:25:56 +0800

    提交测试

--- src/view/demo1.vue
+++ src/view/demo1.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>demo1</div>
+    <div>demo1 {{ a }}</div>
 </template>
 <script setup lang="ts">
-
+import { ref } from "vue"
+const a = ref("123")
 </script>
 <style>
 </style>
